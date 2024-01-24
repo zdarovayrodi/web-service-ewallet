968990698c6f514b7146531127f427da61cd8dd6
Mikhail Saveliev <zdarovayrodi@icloud.com>
2024-01-24 20:19:42 +0300

    refactor(feat): add DB & GET, POST methods

--- main.go
+++ main.go
@@ -3,48 +3,60 @@ package main
 import (
 	"net/http"
 
+	"web-service-ewallet/db"
 	"web-service-ewallet/db/models"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-var wallets = []models.Wallet{
-	{ID: "1", Balance: 56.01},
-	{ID: "2", Balance: 156.02},
-	{ID: "3", Balance: 1999887799956.03},
-}
+var DB *gorm.DB
 
-// /api/v1/wallets (get all)
+// GET /api/v1/wallets
 func getWallets(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, wallets)
+	models.GetAllWallets(context, DB)
 }
 
-// /api/v1/wallets/{id}
+// GET /api/v1/wallets/{id}
 func getWalletById(context *gin.Context) {
 	id := context.Param("id")
-
-	for _, wallet := range wallets {
-		if wallet.ID == id {
-			context.IndentedJSON(http.StatusOK, wallet)
-			return
-		}
-	}
-	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "wallet not found"})
-}
-
-// /api/v1/wallets (post)
-func postWallet(context *gin.Context) {
-	var newWallet models.Wallet
-
-	if err := context.BindJSON(&newWallet); err != nil {
+	wallet, err := models.GetWallet(DB, id)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": "Wallet not found"})
 		return
 	}
 
-	wallets = append(wallets, newWallet)
-	context.IndentedJSON(http.StatusCreated, newWallet)
+	context.JSON(http.StatusOK, wallet)
+}
+
+// POST /api/v1/wallets
+func postWallet(context *gin.Context) {
+	var newWallet models.Wallet
+
+	if context.Request.ContentLength == 0 {
+		newWallet.Balance = 100
+	} else {
+		// in case "balance" field is frovided
+		if err := context.BindJSON(&newWallet); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+			return
+		}
+	}
+
+	createdWallet, err := models.CreateWallet(DB, &newWallet)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, createdWallet)
 }
 
 func main() {
+	//db
+	DB = db.InitDB()
+
+	// api
 	router := gin.Default()
 	router.GET("/api/v1/wallets", getWallets)
 	router.GET("/api/v1/wallets/:id", getWalletById)
